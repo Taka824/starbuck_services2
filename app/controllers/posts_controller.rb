1ece5bba481ae62dcf7bc17778ddf5f8c1fd2351
Taka824 <takahiro82456@yahoo.co.jp>
2022-07-05 18:47:21 +0900

    途中で変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,28 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.includes(:user).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.boards.build(board_params)
+    if @post.save
+      redirect_to boards_path, success: t('defaults.message.created', item: Post.model_name.human)
+    else
+      flash.now['danger'] = t('defaults.message.not_created', item: Post.model_name.human)
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
