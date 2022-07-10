da38b1b118ef0c79cae094e30b4c1a0bb1e7373a
Taka824 <takahiro82456@yahoo.co.jp>
2022-07-10 10:16:28 +0900

    raty 機能修正　未完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require jquery.raty.js
 window.$ = window.jQuery = require("jquery");
-
-require("packs/jquery.raty")
+require("packs/raty")
