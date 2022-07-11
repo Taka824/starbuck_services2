8d53ef1528ae385ac4deeefb2c005ab9fb0e85ff
Taka824 <takahiro82456@yahoo.co.jp>
2022-07-11 12:04:04 +0900

    コメント機能実装終了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'user_sessions#destroy'
   
   resources :users, only: %i[new create]
-  resources :posts, only: %i[index new create]
+  resources :posts, only: %i[index new create show] do
+    resources :comments, only: %i[create], shallow: true
+  end
   resources :categories, except: [:new, :show]
 end
