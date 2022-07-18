a3b8713ef4e78c379c5fba9c027183643bf0b1a3
Taka824 <takahiro82456@yahoo.co.jp>
2022-07-18 10:32:14 +0900

    カテゴリー追加　退会機能　削除機能の実装終了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_003640) do
+ActiveRecord::Schema.define(version: 2022_07_17_233849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,13 +52,12 @@ ActiveRecord::Schema.define(version: 2022_07_16_003640) do
     t.text "arrange_content", null: false
     t.integer "price"
     t.text "one_point"
-    t.integer "status"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "category_id"
     t.json "images"
     t.float "evaluation"
+    t.string "category"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
