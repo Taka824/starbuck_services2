bb04c25958ce9cd515eaf6b3a30c49cdcf66efaa
Taka824 <takahiro82456@yahoo.co.jp>
2022-08-10 11:05:40 +0900

    swiper ok

--- app/assets/javascripts/post__index.js
+++ app/assets/javascripts/post__index.js
@@ -1,26 +1,20 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const hero = new HeroSlider();
-});
-class HeroSlider{
-  constructor(){
-    this.swiper = new Swiper('.swiper',{
-      loop: true,
-      grabCursor: true,
-      effect: 'coverflow',
-      centeredSlides: true,
-      slidesPerView: 1,
-      speed: 1000,
-      breakpoints: {
-        1024: {
-          slidesPerView: 2,
-        }
-      },
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: false
-      }
-    });
+var swiper = new Swiper('.swiper',{
+  loop: true,
+  grabCursor: true,
+  effect: 'coverflow',
+  centeredSlides: true,
+  slidesPerView: 1,
+  speed: 1000,
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+    }
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
   }
-}
+});
+
 
   
